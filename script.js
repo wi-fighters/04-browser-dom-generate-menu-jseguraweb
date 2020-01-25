5de386f2d045b7cf118a601b1dfca47c7cec43b7
Jaime Segura <jseguraweb@gmail.com>
2020-01-25 12:23:08 +0100

    style document with js

--- script.js
+++ script.js
@@ -51,4 +51,60 @@
                 break;
         }
     });
+
+    // reset properties for the document before I start styling it
+    (reset = () => {
+        document.documentElement.style.scrollBehavior = 'smooth';
+        [...document.querySelectorAll('a')].forEach(a => {
+            a.style.textDecoration = 'none';
+            a.style.display = 'block';
+            a.style.textAlign = 'center';
+            a.style.fontWeight = '600';
+            a.style.color = '#8fb59c';
+        });
+        const allElements = [...document.querySelectorAll('*')];
+        allElements.forEach(el => {
+            el.style.margin = '0';
+            el.style.padding = '0';
+            el.style.fontFamily = 'Verdana, Geneva, Tahoma, sans-serif';
+        });
+    })();
+    
+    // style document
+    (bodyStyle = () => {
+        document.body.style.backgroundColor = '#eddbc3';
+        [...document.body.children].forEach((el, i) => i > 0 ? el.style.padding = '1rem' : false)
+    })();
+
+    (navStyle = () => {
+        ul.style.width = '100%';
+        ul.style.backgroundColor = '#813a26';
+        ul.style.display = 'flex';
+        ul.style.alignItems = 'center';
+        ul.style.justifyContent = 'space-around';
+        ul.style.padding = '1rem';
+        ul.style.listStyle = 'none';
+        listOfItems.forEach(item => {
+            item.children[0].style.color = '#eddbc3';
+        });
+    })();
+
+    (h1Style = () => {
+        const h1 = document.querySelector('h1');
+        h1.style.textAlign = 'center';
+        h1.style.margin = '2rem 0 0';
+        h1.style.color = '#813a26';
+        h1.nextElementSibling.style.textAlign = 'center';
+        h1.nextElementSibling.style.fontStyle = 'italic';
+    })();
+
+    (h2sStyle = () => {
+        const h2s = [...document.querySelectorAll('h2')];
+        h2s.forEach(h2 => {
+            h2.style.textAlign = 'center';
+            h2.style.margin = '2rem 0 1rem';
+            h2.style.color = '#D94C3A';
+        });
+    })();
+
 })();
