d9dd303404ef92c2493955ea9dbba0b0b93db34f
Jaime Segura <jseguraweb@gmail.com>
2020-01-25 13:01:23 +0100

    add button to change light mode

--- script.js
+++ script.js
@@ -72,39 +72,83 @@
     
     // style document
     (bodyStyle = () => {
-        document.body.style.backgroundColor = '#eddbc3';
+        document.body.style.width = '100%';
         [...document.body.children].forEach((el, i) => i > 0 ? el.style.padding = '1rem' : false)
     })();
 
     (navStyle = () => {
         ul.style.width = '100%';
-        ul.style.backgroundColor = '#813a26';
         ul.style.display = 'flex';
         ul.style.alignItems = 'center';
         ul.style.justifyContent = 'space-around';
         ul.style.padding = '1rem';
         ul.style.listStyle = 'none';
-        listOfItems.forEach(item => {
-            item.children[0].style.color = '#eddbc3';
-        });
+        listOfItems.forEach(item => item.children[0].style.color = '#eddbc3');
     })();
 
+    const h1 = document.querySelector('h1');
     (h1Style = () => {
-        const h1 = document.querySelector('h1');
         h1.style.textAlign = 'center';
         h1.style.margin = '2rem 0 0';
-        h1.style.color = '#813a26';
         h1.nextElementSibling.style.textAlign = 'center';
         h1.nextElementSibling.style.fontStyle = 'italic';
     })();
 
+    const h2s = [...document.querySelectorAll('h2')];
     (h2sStyle = () => {
-        const h2s = [...document.querySelectorAll('h2')];
         h2s.forEach(h2 => {
             h2.style.textAlign = 'center';
             h2.style.margin = '2rem 0 1rem';
-            h2.style.color = '#D94C3A';
         });
     })();
 
+    // light-mode button
+    const dayMode = () => {
+        document.body.style.backgroundColor = '#eddbc3';
+        ul.style.backgroundColor = '#813a26';
+        h1.style.color = '#813a26';
+        h1.nextElementSibling.style.color = '#000';
+        h2s.forEach(h2 => h2.style.color = '#D94C3A');
+        listOfItems.forEach(item => item.children[0].style.color = '#05080D');
+        [...document.querySelectorAll('p')].forEach(p => p.style.color = '#000');
+    };
+
+    const nightMode = () => {
+        document.body.style.backgroundColor = '#05080D';
+        ul.style.backgroundColor = '#E7E3E2';
+        h1.style.color = '#E7E3E2';
+        h1.nextElementSibling.style.color = '#fff';
+        h2s.forEach(h2 => h2.style.color = '#DEA26C');
+        listOfItems.forEach(item => item.children[0].style.color = '#05080D');
+        [...document.querySelectorAll('p')].forEach(p => p.style.color = '#fff');
+    };
+
+    dayMode();
+    (changeModeButton = () => {
+        const modeButton = document.createElement('button');
+        document.body.insertBefore(modeButton, h1);
+        modeButton.innerText = 'Change Light Mode';
+        modeButton.style.border = 'none';
+        modeButton.style. padding = '1rem';
+        modeButton.style.cursor = 'pointer';
+        modeButton.style.backgroundColor = 'grey';
+        modeButton.style.color = '#fff';
+        modeButton.style.fontWeight = '600';
+        modeButton.style.display = 'block';
+        modeButton.style.margin = '0.5rem auto 0.5rem';
+        modeButton.style.borderRadius = '10px';
+        modeButton.style.boxShadow = '0px 0px 5px black';
+        modeButton.addEventListener('mouseover', (e) => e.target.style.backgroundColor = 'rgb(80, 80, 80)');
+        modeButton.addEventListener('mouseout', (e) => e.target.style.backgroundColor = 'grey');
+        let button = 0;
+        modeButton.addEventListener('click', (e) => {
+            if (button === 0) {
+                nightMode();
+                button = 1;
+            } else { 
+                dayMode();
+                button = 0;
+            };
+        });
+    })();
 })();
